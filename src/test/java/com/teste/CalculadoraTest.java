a1cc1e6e5a5ba7ebb1150671c4a27a23f3a72561
Lucas Gomes <lucas.amocristo@gmail.com>
2021-03-10 09:59:53 -0300

    Mocks com Mockito

--- src/test/java/com/teste/CalculadoraTest.java
+++ src/test/java/com/teste/CalculadoraTest.java
@@ -1,6 +1,7 @@
 package com.teste;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 
@@ -12,6 +13,17 @@ public class CalculadoraTest {
 		int soma=calc.somar("1+1+3");
 		
 		assertEquals(5, soma);
+	}
+	
+	@Test
+	public void testeSomaComMock() {
+		Calculadora calc= mock(Calculadora.class);
+		
+		when(calc.somar("1+2")).thenReturn(10);
+		
+		int resultado = calc.somar("1+2");
+		
+		assertEquals(10, resultado);
 	}
 
 }
